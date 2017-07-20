22b0a388733336b693881b326ca4e7e635e799ee
Rob Stocker <djtechbot@gmail.com>
2017-07-20 14:34:50 +0200

    Delete categories.js

--- app/models/categories.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-    name : DS.attr('string'),
-    description: DS.attr('string'),
-    path: DS.attr('string'),
-    weight: DS.attr(),
-    recipes: DS.hasMany('recipes', {inverse: 'category'})
-});
