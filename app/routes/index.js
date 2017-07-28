6a75a35ab882bc0fb8fa15d6f8f768146fe8e056
Lisa Corcoran <lcdemc@gmail.com>
2017-07-28 20:11:52 +0200

    disable recipes route set to front page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    beforeModel() {
-        this.replaceWith('recipes');
-    }
+
+  //beforeModel() {
+  //    this.replaceWith('recipes');
+  //}
+
+  model() {
+    return Ember.RSVP.hash({
+      categories: this.store.findAll('category'),
+      recipes:  this.store.query('recipe', {
+        sort: "-createdAt",
+        page: {
+          limit: 30,
+        },
+        filter: {}
+      }),
+    });
+  },
+
+  setupController(controller, models) {
+    controller.set('categories', models.categories);
+    controller.set('recipes', models.recipes);
+  },
+
+  afterModel() {
+    console.log(this.get('routeName'));
+  }
+
 });
